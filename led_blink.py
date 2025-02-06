cda460b26ff39f31995c6c3ad8e465fddf872cff
grzesjerzy <g.olewczynski@gmail.com>
2025-02-06 22:03:26 +1100

    Create led blinking file to test GIT

--- led_blink.py
+++ led_blink.py
@@ -6,7 +6,8 @@ GPIO.setup(8, GPIO.OUT)
 GPIO.setup(10, GPIO.OUT)
 
 GPIO.output(8, GPIO.HIGH)
+GPIO.output(10, GPIO.HIGH)
 sleep(2)
-GPIO.output(8, GPIO.LOW)
+GPIO.output((8, 10), GPIO.LOW)
 
 GPIO.cleanup()
